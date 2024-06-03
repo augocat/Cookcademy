06b1dcf12bf1a49a4be3b84136452f5e7a2e9b4c
narb <narborough.grahame@icloud.com>
2024-06-04 09:18:53 +0930

    Initial Commit

---
+++ Cookcademy/CookcademyApp.swift
@@ -0,0 +1,17 @@
+//
+//  CookcademyApp.swift
+//  Cookcademy
+//
+//  Created by Grahame Narborough on 4/6/2024.
+//
+
+import SwiftUI
+
+@main
+struct CookcademyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
